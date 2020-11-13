5b271192c2ec6b96b7a5b130237138375dd394eb
Danny Smooke <dana.smooke@gmail.com>
2020-11-13 13:53:06 -0500

    defined selection variables with html ids, defined constants(-El) with what each includes

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Instructions: Assignment Code
+var generateBtn     = document.querySelector("#generate");        //refers to html btn id 
+var lengthEl        = document.querySelector("#passwordLength");  //refers to password length id
+var lowercaseEl     = document.querySelector("#LowerCase");       //refers to html lowercase id
+var uppercaseEl     = document.querySelector("#UpperCase");       //refers to html uppercase id
+var numbersEl       = document.querySelector("#Numbers");         //refers to html numbers id
+var symbolsEl       = document.querySelector("#Symbols");         //refers to html symbols id
+var newPassword     = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
+const lowercaseEl = ["abcdefghijklmnopqrstuvwxyz"]; //what lowercaseEl contains
+const uppercaseEl = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; //what uppercaseEl contains
+const numbersEl   = ["0123456789"];                 //what numbersEl contains
+const symbolsEl   = ["!@#$%^&*+={}[]()/?<>"];       //what symbolsEl contains
+
+let criteria = [lengthEl, lowercaseEl, uppercaseEl, numbersEl, symbolsEl]; //criteria must be 
+
+
+
+
+// Instructions: Write password to the #password input
+function writePassword() { //what do you want to happen?
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,5 +26,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Instructions: Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
