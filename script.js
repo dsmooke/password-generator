c63b6163602dc94996d041d4785bb8099208c272
Danny Smooke <dana.smooke@gmail.com>
2020-11-14 20:03:58 -0500

    makeover, ta chris major help - could not have done it without him

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
 var generateBtn     = document.querySelector("#generate");        //refers to html btn id 
 
-const lengthEl = (document.querySelector("#passwordLength"));
-const lowercaseEl = "abcdefghijklmnopqrstuvwxyz"; //what lowercaseEl contains 26 letters
-const uppercaseEl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //what uppercaseEl contains 26 letters
-const numbersEl   = "0123456789";                 //what numbersEl contains 10 numbers
-const symbolsEl   = "!@#$%^&*+={}[]()/?<>";       //what symbolsEl contains ~20 symbols
+const lowercase   = "abcdefghijklmnopqrstuvwxyz"; //what lowercaseEl contains 
+const uppercase   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //what uppercaseEl contains 
+const numbers     = "0123456789";                 //what numbersEl contains
+const symbols     = "!@#$%^&*+={}[]()/?<>";       //what symbolsEl contains
 
-for (var i =0; i > 8, i <= 128; i++) {
-  console.log(lengthEl);
-  //want to use length and apply to all 
-}
-
-for (var i =0; i < 10; i++) {
-  console.log(uppercaseEl[(Math.floor(Math.random()) * uppercaseEl.length)]);
-}
-
-for (var i =0; i < 10; i++) {
-  console.log(lowercaseEl[(Math.floor(Math.random()) * lowercaseEl.length)]);
-}
-
-for (var i =0; i < 10; i++) {
-  console.log(numbersEl[(Math.floor(Math.random()) * numbersEl.length)]);
-}
-
-for (var i =0; i < 10; i++) {
-  console.log(symbolsEl[(Math.floor(Math.random()) * symbolsEl.length)]);
-}
-
-
-// Instructions: Write password to the #password input
-function writePassword() { //what do you want to happen?
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+//taking user inputs and passing them through generatePassword function
+function generatePassword() {
   
+  //taking user inputs and passing them through generatePassword function
+
+  const passwordLength = document.querySelector("#passwordLength").value; //length chosed
+  console.log(passwordLength);
+
+  var includeLowercase = document.querySelector("#LowerCase").checked; //wants lowercase
+  console.log(includeLowercase);
+
+  var includeUppercase = document.querySelector("#UpperCase").checked; //wants uppercase
+  console.log(includeUppercase);
+
+  var includeNumbers = document.querySelector("#Numbers").checked; //wamts numbers
+  console.log(includeNumbers);
+
+  var includeSymbols = document.querySelector("#Symbols").checked; //wants symbols
+  console.log(includeSymbols);
+  
+  var passwordText = document.querySelector("#password"); // where combined inputs will be displayed
+
+
+  var password = ""; // characters pulled from lottery
+  var poolString = ""; // lottery pool of random characters
+
+
+
+    if (includeLowercase === true) {    // if checked, add lowercase items into lottery pool (poolString)
+      poolString = poolString.concat(lowercase)
+    } 
+    
+    if (includeUppercase === true) {   // if checked, add lowercase items into lottery pool (poolString)
+      poolString = poolString.concat(uppercase)
+    } 
+
+    if (includeNumbers === true) {     // if checked, add lowercase items into lottery pool (poolString)
+      poolString = poolString.concat(numbers)
+    } 
+
+    if (includeSymbols === true) {    // if checked, add lowercase items into lottery pool (poolString)
+      poolString = poolString.concat(symbols)
+    } 
+
+
+  for (var i = 0; i < passwordLength; i++) { //loop, will run as many times as user's length input will stop once reaches user's password length
+    
+    var randomIndex = Math.floor(Math.random() * poolString.length); //random index within length
+    var randomCharacter = poolString.charAt(randomIndex); //the chose character from randomIndex
+    
+    password = password.concat(randomCharacter); //password now has the chosen characters
+  }
+
+  passwordText.value = password; //rendering password to browser (you will see it in box)
+ 
+  console.log(password); // rendered password shown in console
 }
 
-// Instructions: Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
 
